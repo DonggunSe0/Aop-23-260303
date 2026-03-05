ccd353ad86fdff4bab83c2e02d5bb4bff8b40dae
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-05 17:35:00 +0900

    4 component/ 의존성 주입

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,19 @@
 package com.back;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration //configuration 어노테이션을 사용하여 AppConfig 클래스를 설정 클래스(Bean 설정 클래스)로 지정
 public class AppConfig {
 
-    @Bean // Bean으로 PersonService 객체를 등록
-    public PersonService personService() {
-        System.out.println("AppConfig.personService() 호출");
-        return new PersonService();
-    }
+
+
+
+
+    //PersonService 클래스에 @Component 어노테이션이 붙어 있기 때문에
+    // AppConfig 클래스에서 Bean으로 등록할 필요가 없어짐
+//    @Bean // Bean으로 PersonService 객체를 등록
+//    public PersonService personService() {
+//        System.out.println("AppConfig.personService() 호출");
+//        return new PersonService();
+//    }
 }
--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,5 +1,9 @@
 package com.back;
 
+import org.springframework.stereotype.Service;
+
+@Service//Component 어노테이션을 사용하여 PersonService 클래스를 Bean으로 등록
+//그래서 AppConfig 클래스에서 Bean으로 등록할 필요가 없어짐
 public class PersonService {
     public int count() {
         return 3;
