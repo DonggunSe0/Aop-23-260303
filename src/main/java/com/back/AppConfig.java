d67595071299817488a5086172f077d8d5e3e539
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-06 10:29:47 +0900

    5-1 version manage

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,13 +1,23 @@
 package com.back;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration //configuration 어노테이션을 사용하여 AppConfig 클래스를 설정 클래스(Bean 설정 클래스)로 지정
 public class AppConfig {
 
 
+    //PersonRepository에 component이용하지 않고 버전 관리를 위해서 Bean으로 등록
+    //그러므로 상황에서 맞게 Componet를 쓸지 Bean으로 등록할지 선택하면 될듯
+    @Bean
+    public PersonRepository personRepository() {
+        return new PersonRepository(1);
+    }
 
-
+    @Bean
+    public PersonRepository personRepositoryV2() {
+        return new PersonRepository(2);
+    }
 
     //PersonService 클래스에 @Component 어노테이션이 붙어 있기 때문에
     // AppConfig 클래스에서 Bean으로 등록할 필요가 없어짐
--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,11 +1,15 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service//Component 어노테이션을 사용하여 PersonService 클래스를 Bean으로 등록
+@Service
+@RequiredArgsConstructor//Component 어노테이션을 사용하여 PersonService 클래스를 Bean으로 등록
 //그래서 AppConfig 클래스에서 Bean으로 등록할 필요가 없어짐
 public class PersonService {
+    private final PersonRepository personRepository; //의존성 주입(DI)을 통해 PersonRepository 객체를 주입받음
+
     public int count() {
-        return 3;
+        return personRepository.count(); //PersonRepository의 count() 메서드를 호출하여 사람 수를 반환
     }
 }
