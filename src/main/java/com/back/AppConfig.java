d8f6a4f6e35ed2c7371cc61cea3e7789db23cd67
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-07 00:50:16 +0900

    6

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration //configuration 어노테이션을 사용하여 AppConfig 클래스를 설정 클래스(Bean 설정 클래스)로 지정
 public class AppConfig {
-
-
     //PersonRepository에 component이용하지 않고 버전 관리를 위해서 Bean으로 등록
     //그러므로 상황에서 맞게 Componet를 쓸지 Bean으로 등록할지 선택하면 될듯
     @Bean
@@ -25,5 +23,10 @@ public class AppConfig {
 //    public PersonService personService() {
 //        System.out.println("AppConfig.personService() 호출");
 //        return new PersonService();
+//    }
+
+//    public ApplicationRunner myApplicationRunner() {
+////        return new ApplicationRunner(); //new가 안되는 이유 ->
+//        // ApplicationRunner는 스프링에서 제공하는 인터페이스이기 때문에 직접 인스턴스를 생성할 수 없어.
 //    }
 }
