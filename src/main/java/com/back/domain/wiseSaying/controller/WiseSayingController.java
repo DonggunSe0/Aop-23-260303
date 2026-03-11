e9139ab5468f1ae8d24df4e99110420f2b6e234e
DonggunSe0 <cloudhenry1@gmail.com>
2026-03-11 16:29:47 +0900

    21강까지 update/delete추가 service controller 분리

--- src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/domain/wiseSaying/controller/WiseSayingController.java
@@ -1,30 +1,30 @@
 package com.back.domain.wiseSaying.controller;
 
 import com.back.domain.wiseSaying.entity.WiseSaying;
+import com.back.domain.wiseSaying.service.WiseSayingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
-public class wiseSayingController {
-    private final List<WiseSaying> wiseSayingList = new ArrayList<>();
-    private int lastId = 0;
+@RequiredArgsConstructor // final이 붙은 필드의 생성자를 만들어주는 어노테이션, 생성자 주입 방식으로 의존성 주입을 할 때 사용
+public class WiseSayingController {
+    private final WiseSayingService wiseSayingService;
 
     //queryString 방식으로 명언 등록
-
-    @GetMapping("/wiseSaying/write")
+    @GetMapping("/wiseSayings/write")
     @ResponseBody
     public String write(
             String content,
             String author
             //write?content=aaa&author=bbb 쪼개서 content=aaa, author=bbb
-    ) { // 명언 등록 create
+    ) {
+        // 명언 등록 create
 
         if (content == null || content.trim().length() == 0) {
             throw new RuntimeException("명언을 입력해주세요.");
@@ -34,15 +34,14 @@ public class wiseSayingController {
             throw new RuntimeException("작가를 입력해주세요.");
         } // 보통 스프링에서 유효성 체크를 예외처리로
 
-        WiseSaying wiseSaying = new WiseSaying(++lastId,content, author);
-        wiseSayingList.add(wiseSaying);
+        WiseSaying wiseSaying = wiseSayingService.write(content, author);
 
         return "%d번 명언이 등록되었습니다.".formatted(wiseSaying.getId());
     }
-    @GetMapping("/wiseSaying/list")
+    @GetMapping("/wiseSayings")
     @ResponseBody
     public String list() { //명언 목록
-        String wiseSayings = wiseSayingList.stream()
+        String wiseSayings = wiseSayingService.findAll().stream()
                 .map(w -> "<li>%s / %s / %s </li>".formatted(w.getId(), w.getContent(), w.getAuthor()))
                 .collect(Collectors.joining("\n"));
 
@@ -52,24 +51,60 @@ public class wiseSayingController {
                 </ul>""".formatted(wiseSayings);
     }
 
-    @GetMapping("/wiseSaying/delete/{id}") //변수화
+
+    @GetMapping("/wiseSayings/{id}") //주소체계
+    @ResponseBody
+    public String detail(
+            @PathVariable int id
+
+    ) {
+
+        WiseSaying wiseSaying = wiseSayingService.findById(id);
+        return """
+                <h1>번호 : %s</h1>
+                <div>명언 : %s</div>
+                <div>작가 : %s</div>
+                """.formatted(wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor());
+    }
+
+    @GetMapping("/wiseSayings/delete/{id}") //변수화
     @ResponseBody // 삭제?id=1 -> v1
     public String delete(
             @PathVariable int id
     ) {
-        Optional<WiseSaying> wiseSaying = wiseSayingList.stream()
-                .filter(w->w.getId() == id)
-                .findFirst();
+        WiseSaying wiseSaying = wiseSayingService.findById(id);
 
-        if (wiseSaying.isEmpty()) {
-            throw new RuntimeException("%d번 명언은 존재하지 않습니다.".formatted(id));
-        }
-
-        wiseSayingList.remove(wiseSaying.get());
+     //   wiseSayingList.remove(wiseSaying);
+        wiseSayingService.delete(wiseSaying);
 
         return "%d번 명언이 삭제되었습니다.".formatted(id);
 
 
     }
+//update 수정기능 구현
+    @GetMapping("/wiseSayings/modify/{id}")
+    @ResponseBody
+    public String modify(
+            @PathVariable int id,
+            @RequestParam(defaultValue = "기본값") String content,
+            @RequestParam(defaultValue = "기본값") String author
+            //예외 처리 대신 defaultValue를 기본값으로 주고 할 수 있음
+    ) {
+//        if (content == null || content.trim().length() == 0) {
+//        throw new RuntimeException("명언을 입력해주세요.");
+//        } // 보통 스프링에서 유효성 체크를 예외처리로
+//
+//        if (author == null || author.trim().length() == 0) {
+//            throw new RuntimeException("작가를 입력해주세요.");
+//        } // 보통 스프링에서 유효성 체크를 예외처리로
+
+
+        WiseSaying wiseSaying = wiseSayingService.findById(id);
+        wiseSayingService.modify(wiseSaying, content, author);
+
+        return "%d번 명언이 수정되었습니다.".formatted(id);
+    }
+
+
 
 }
